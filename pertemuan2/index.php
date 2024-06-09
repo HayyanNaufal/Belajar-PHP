12e469e57cdeaa6bebf944f4acb38b2f75d8087a
HayyanNaufal <hayyan325@gmail.com>
2024-06-09 09:39:00 +0700

    pertemuan 2 "sintaks php"

---
+++ pertemuan2/index.php
@@ -0,0 +1,47 @@
+<?php 
+// Sintaks PHP
+
+// output
+// echo, print, print_r, var_dump
+// var_dump("Hayyan Naufal");
+
+// Penulisan sintaks php
+// 1. php di dalam html
+// 2. html di dalam php
+
+// variabel dan tipe data
+// variabel "$"
+// $nama = "hayyan";
+
+
+// Operator 
+// aritmatika
+// +, -, *, /, %
+
+// concat / concatenation/ penggabung string
+// .
+// $a = "Hayyan";
+// $b = "Naufal";
+// echo $a . " " . $b;
+
+// penugasan / assignment
+// = , +=, -=, *=, /=, %=, .=
+// $x = 3;
+// $x += 2;
+// echo $x;
+
+// perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// logika
+// &&, ||, !
+// $x = "10";
+// var_dump( $x > 5 && $x % 2 == 0);
+
+
+?>
